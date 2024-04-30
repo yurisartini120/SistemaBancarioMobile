c399e2480385fd9a1d72fc356d8299d440dda57a
Kaua Fonseca <137219893+KauaFonsecaDeCarvalho@users.noreply.github.com>
2024-04-30 14:32:58 -0300

    1

---
+++ SistemaBancarioMobile/app/src/main/java/DevAndroid/SistemaBancarioMobile/telaSplash.java
@@ -0,0 +1,14 @@
+package DevAndroid.SistemaBancarioMobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_splash);
+    }
+}
