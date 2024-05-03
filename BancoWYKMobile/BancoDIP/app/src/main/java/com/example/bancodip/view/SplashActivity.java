a76d1ac81ef184a5993944c77ab911887a8afacb
Kaua Fonseca <137219893+KauaFonsecaDeCarvalho@users.noreply.github.com>
2024-05-03 16:46:18 -0300

    finalização do layout

--- BancoWYKMobile/BancoDIP/app/src/main/java/com/example/bancodip/view/SplashActivity.java
+++ BancoWYKMobile/BancoDIP/app/src/main/java/com/example/bancodip/view/SplashActivity.java
@@ -10,7 +10,7 @@ import com.example.bancodip.R;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 5000;
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
